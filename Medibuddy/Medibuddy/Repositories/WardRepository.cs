ac6e35dfe7cd1621b4dba3fcde5c213cd70709c0
Suresh-Mante <99386236+Suresh-Mante@users.noreply.github.com>
2022-09-10 14:26:10 +0530

    Created Ward Api
    Implementation is not completed yet. It's just for everyone's reference. You all may start implementing it.
    
    As it's first branch I am merging it by myself.

---
+++ Medibuddy/Medibuddy/Repositories/WardRepository.cs
@@ -0,0 +1,44 @@
+﻿using Medibuddy.DataAccess;
+using Medibuddy.Models;
+
+namespace Medibuddy.Repositories
+{
+    public class WardRepository : IWardRepository
+    {
+        private readonly IWardDataAccess _wardDataAccess;
+
+        public WardRepository(IWardDataAccess wardDataAccess)
+        {
+            _wardDataAccess = wardDataAccess;
+        }
+        public Ward Create(Ward ward)
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+
+        public Ward Delete(int id)
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+
+        public Ward Get(int id)
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ward> Get()
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+
+        public Ward Update(int id, Ward ward)
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+    }
+}
