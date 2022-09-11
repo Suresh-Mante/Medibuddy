3907b728895881c3c538a49373783960d5f84306
Ajay Bohra <63140274+iamajaybohra@users.noreply.github.com>
2022-09-11 17:20:26 +0530

    Add Utils

---
+++ Medibuddy/Medibuddy/Utils/ErrorMessages.cs
@@ -0,0 +1,9 @@
+namespace Medibuddy.Utils
+{
+    public class ErrorMessages
+    {
+        public const string Required = "Required field";
+        public const string RequiredPositiveNumber = "Only positive values are allowed";
+        public const string RequiredNonNegativeNumber = "Negative numbers are not allowed";
+    }
+}
