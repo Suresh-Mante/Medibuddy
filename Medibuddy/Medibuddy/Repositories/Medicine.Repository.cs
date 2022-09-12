6dd483c51bf26a6f5d2fa2b6408bca2a4bc321ce
gaffur11 <ravibudania59@gmail.com>
2022-09-12 10:45:44 +0530

    created medicine api structure

---
+++ Medibuddy/Medibuddy/Repositories/Medicine.Repository.cs
@@ -0,0 +1,44 @@
+﻿using Medibuddy.DataAccess;
+using Medibuddy.Models;
+
+namespace Medibuddy.Repositories
+{
+    public class MedicineRepository
+    {
+        private readonly IMedicineDataAccess _medicineDataAccess;
+
+        public MedicineRepository(IMedicineDataAccess medicineDataAccess)
+        {
+            _medicineDataAccess = medicineDataAccess;
+        }
+        public Medicine Create(Medicine medicine)
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+
+        public Medicine Delete(int Id)
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+
+        public Medicine Get(int Id)
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Medicine> Get()
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+
+        public Medicine Update(int Id, Medicine test)
+        {
+            //Write your implementation here
+            throw new NotImplementedException();
+        }
+    }
+}
