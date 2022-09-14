64ce6bc860ca2da1794d96b3819372c316bca03f
Suresh-Mante <99386236+Suresh-Mante@users.noreply.github.com>
2022-09-14 22:12:50 +0530

    Complete implementation of OPTPatientAPI

--- Medibuddy/Medibuddy/Program.cs
+++ Medibuddy/Medibuddy/Program.cs
@@ -29,6 +29,10 @@ builder.Services.AddScoped<IDoctorDataAccess, DoctorDataAccess>();
 builder.Services.AddScoped<INurseRepository, NurseRepository>();
 builder.Services.AddScoped<INurseDataAccess, NurseDataAccess>();
 
+//For OPDPatient Api
+builder.Services.AddScoped<IOPDPatientRepository, OPDPatientRepository>();
+builder.Services.AddScoped<IOPDPatientDataAccess, OPDPatientDataAccess>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
