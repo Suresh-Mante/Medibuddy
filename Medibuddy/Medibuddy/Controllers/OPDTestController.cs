87bef0fd1e881d8ac11eec35e9ecfe2d25bfb441
Ajay Bohra <63140274+iamajaybohra@users.noreply.github.com>
2022-09-14 08:27:52 +0530

    controllerVersion1_opd_api

---
+++ Medibuddy/Medibuddy/Controllers/OPDTestController.cs
@@ -0,0 +1,58 @@
+﻿using Medibuddy.Models;
+using Medibuddy.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medibuddy.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OPDTestController : Controller
+    {
+
+        private readonly IOPDTestRepository _opdtestRepository;
+
+        public OPDTestController(IOPDTestRepository opdtestRepository)
+        {
+            _opdtestRepository = opdtestRepository;
+        }
+
+        [HttpPost]
+        public async Task<Response<OPDTest>> Create(OPDTestDTO opdtest)
+        {
+            OPDTest newOPDTest = new OPDTest()
+            {
+                OPDBillingID = opdtest.OPDBillingID,
+                TestID = opdtest.TestID
+            };
+            return await _opdtestRepository.Create(newOPDTest);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Response<OPDTest>> Get(int OPDBillingID,int TestID)
+        {
+            return await _opdtestRepository.Get(OPDBillingID,TestID);
+        }
+
+        [HttpGet]
+        public async Task<Response<OPDTest>> Get()
+        {
+            return await _opdtestRepository.Get();
+        }
+
+        [HttpPut]
+        public async Task<Response<OPDTest>> Update(int OPDBillingID,int TestID, OPDTestDTO opdtest)
+        {
+            return await _opdtestRepository.Update(OPDBillingID,TestID, new OPDTest()
+            {
+                OPDBillingID = opdtest.OPDBillingID,
+                TestID = opdtest.TestID
+            });
+        }
+
+        [HttpDelete]
+        public async Task<Response<OPDTest>> Delete(int OPDBillingID,int TestID)
+        {
+            return await _opdtestRepository.Delete(OPDBillingID,TestID);
+        }
+    }
+}
