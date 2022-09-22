1f3aed3941f17f85c43c4f38b408c35da0530365
Soudipdas <aparnadas1211981@gmail.com>
2022-09-22 11:52:39 +0530

    Model, DTO, Controller, Repository implemented

---
+++ Medibuddy/Medibuddy/Repositories/IIPDPatientRepository.cs
@@ -0,0 +1,14 @@
+﻿using Medibuddy.Models;
+
+namespace Medibuddy.Repositories
+{
+    public interface IIPDPatientRepository  
+    {
+        public Task<Response<IPDPatient>> Create(IPDPatient ipdpatient);
+        public Task<Response<IPDPatient>> Get(int id);
+        public Task<Response<IPDPatient>> Get();
+        public Task<Response<IPDPatient>> Update(int id, IPDPatient ipdpatient);
+        public Task<Response<IPDPatient>> Delete(int id);
+        public Task<Response<IPDPatient>> Discharge(int id);
+    }
+}
---
+++ Medibuddy/Medibuddy/Repositories/IPDPatientRepository.cs
@@ -0,0 +1,185 @@
+﻿using Medibuddy.Models;
+using Medibuddy.Utils;
+
+namespace Medibuddy.Repositories
+{
+    public class IPDPatientRepository : IIPDPatientRepository
+    {
+        private readonly IIPDPatientDataAccess _IPDPatientDataAccess;
+
+        public IPDPatientRepository(IIPDPatientDataAccess IPDPatientDataAccess)
+        {
+            _IPDPatientDataAccess = IPDPatientDataAccess;
+        }
+
+        public async Task<Response<IPDPatient>> Create(IPDPatient IPDPatient)
+        {
+            Response<IPDPatient> response = new Response<IPDPatient>();
+
+            try
+            {
+                IPDPatient createdIPDPatient = await _IPDPatientDataAccess.Create(IPDPatient);
+                response.StatusCode = 201;
+                response.StatusMessage = HttpMessages.Created;
+                response.Record = createdIPDPatient;
+            }
+            catch (Exception ex)
+            {
+                //Write logic to log this exceptions somewhere//
+
+                response.StatusCode = 500;
+                //response.StatusMessage = HttpMessages.InternalServerError;
+                response.StatusMessage = ex.Message;
+            }
+
+            return response;
+        }
+
+        public async Task<Response<IPDPatient>> Delete(int id)
+        {
+            Response<IPDPatient> response = new Response<IPDPatient>();
+
+            try
+            {
+                IPDPatient? existingIPDPatient = await _IPDPatientDataAccess.Get(id);
+                if (existingIPDPatient != null)
+                {
+                    await _IPDPatientDataAccess.Delete(id);
+                    response.StatusCode = 200;
+                    response.StatusMessage = HttpMessages.Deleted;
+                    response.Record = existingIPDPatient;
+                }
+                else
+                {
+                    response.StatusCode = 404;
+                    response.StatusMessage = HttpMessages.NotFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Write logic to log this exceptions somewhere//
+
+                response.StatusCode = 500;
+                response.StatusMessage = HttpMessages.InternalServerError;
+            }
+
+            return response;
+        }
+
+        public async Task<Response<IPDPatient>> Discharge(int id)
+        {
+            Response<IPDPatient> response = new Response<IPDPatient>();
+
+            try
+            {
+                IPDPatient? existingIPDPatient = await _IPDPatientDataAccess.Get(id);
+                if (existingIPDPatient != null)
+                {
+                    existingIPDPatient.Discharged = true;
+                    await _IPDPatientDataAccess.Update(id, existingIPDPatient);
+                    response.StatusCode = 200;
+                    response.StatusMessage = HttpMessages.Deleted;
+                    response.Record = existingIPDPatient;
+                }
+                else
+                {
+                    response.StatusCode = 404;
+                    response.StatusMessage = HttpMessages.NotFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Write logic to log this exceptions somewhere//
+
+                response.StatusCode = 500;
+                response.StatusMessage = HttpMessages.InternalServerError;
+            }
+
+            return response;
+        }
+
+        public async Task<Response<IPDPatient>> Get(int id)
+        {
+            Response<IPDPatient> response = new Response<IPDPatient>();
+
+            try
+            {
+                IPDPatient? IPDPatient = await _IPDPatientDataAccess.Get(id);
+                if (IPDPatient != null)
+                {
+                    response.StatusCode = 200;
+                    response.StatusMessage = HttpMessages.Ok;
+                    response.Record = IPDPatient;
+                }
+                else
+                {
+                    response.StatusCode = 404;
+                    response.StatusMessage = HttpMessages.NotFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Write logic to log this exceptions somewhere//
+
+                response.StatusCode = 500;
+                response.StatusMessage = HttpMessages.InternalServerError;
+            }
+
+            return response;
+        }
+
+        public async Task<Response<IPDPatient>> Get()
+        {
+            Response<IPDPatient> response = new Response<IPDPatient>();
+
+            try
+            {
+                List<IPDPatient> IPDPatients = (await _IPDPatientDataAccess.Get()).ToList();
+                response.StatusCode = 200;
+                response.StatusMessage = HttpMessages.Ok;
+                response.Records = IPDPatients;
+            }
+            catch (Exception ex)
+            {
+                //Write logic to log this exceptions somewhere//
+
+                response.StatusCode = 500;
+                response.StatusMessage = HttpMessages.InternalServerError;
+            }
+
+            return response;
+        }
+
+        public async Task<Response<IPDPatient>> Update(int id, IPDPatient IPDPatient)
+        {
+            Response<IPDPatient> response = new Response<IPDPatient>();
+
+            try
+            {
+                IPDPatient? existingIPDPatient = await _IPDPatientDataAccess.Get(id);
+                if (existingIPDPatient != null)
+                {
+                    IPDPatient? updatedIPDPatient = await _IPDPatientDataAccess.Update(id, IPDPatient);
+                    response.StatusCode = 204;
+                    response.StatusMessage = HttpMessages.Updated;
+                    response.Record = updatedIPDPatient;
+                }
+                else
+                {
+                    response.StatusCode = 404;
+                    response.StatusMessage = HttpMessages.NotFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Write logic to log this exceptions somewhere//
+
+                response.StatusCode = 500;
+                response.StatusMessage = HttpMessages.InternalServerError;
+            }
+
+            return response;
+        }
+
+    }
+}
